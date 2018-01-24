0d8e4604839bf9702fc83d52a591a5c26df3a43d
Sandra <sandimee9@gmail.com>
2018-01-24 14:21:41 +0800

    create Player class

---
+++ TypespeedGame/src/Shooter.java
@@ -0,0 +1,13 @@
+
+public class Shooter {
+
+	public Shooter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
