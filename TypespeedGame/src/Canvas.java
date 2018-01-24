7e71f36e6d5d75e6ff73202d941a2001165e4483
Sandra <sandimee9@gmail.com>
2018-01-24 15:36:38 +0800

    Canvas class update 1

--- TypespeedGame/src/Canvas.java
+++ TypespeedGame/src/Canvas.java
@@ -1,13 +1,40 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
-public class Canvas {
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 
-	public Canvas() {
-		// TODO Auto-generated constructor stub
-	}
+
+//Starting code copied from: https://github.com/tutsplus/Introduction-to-JavaFX-for-Game-Development/blob/master/Example3T.java 
+
+public class Canvas extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
+	}
+	
+	@Override 
+	public void start(Stage theStage) {
+		theStage.setTitle("This is title");
+		
+		Group root = new Group();
+		Scene the Scene = new Scene(root);
+		theStage.setScene(theScene);
+		
+		Canvas canvas = new Canvas(512,512);
+		root.getChildren().add(canvas);
+		
+		//link to GameElements
+		
+		
 	}
 
 }
