5d12d78f4bd7e7a9850af86d2fea56668c055907
tinlongalex <35757063+tinlongalex@users.noreply.github.com>
2018-01-25 10:23:26 +0800

    Update Target.java

--- TypespeedGame/src/Target.java
+++ TypespeedGame/src/Target.java
@@ -1,13 +1,45 @@
+import java.lang.*;
+import java.util.*;
 
-public class Target {
-
+public class Target implements Runnable{
+	
+	//Declare variables for the class
+	public double xCor;
+	public double yCor;
+	public String[] wordsOnScreen = new String[10000];
+	public String[] wordBank = new String[10000];
+	public double speed;
+	
+	
 	public Target() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	//Override the run() method in Runnable interface
+	public void run() {
+	
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	//Scan word from the text file
+	public static void scanFile() throws IOException {
+		//Declare scanner to scan the words.txt file
+		Scanner scannerForTextFile = new Scanenr(new File(words.txt));
+		
+	}
+	
+	//Get method for X coordinate
+	public static double getXCor() {
+		return xCor;
+	}
+	
+	//Get method for Y coordinate
+	public static double getYCor() {
+		return yCor;
 	}
 
 }
